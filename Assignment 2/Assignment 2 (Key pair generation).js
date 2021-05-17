2f833f151999d208d37ceadcf319837a60df1312
mandar7-git <82501714+mandar7-git@users.noreply.github.com>
2021-05-18 00:28:22 +0530

    Add files via upload

---
+++ Assignment 2/Assignment 2 (Key pair generation).js
@@ -0,0 +1,32 @@
+
+var crypto = require('crypto');
+
+var fs = require('fs');       // to work with files
+const path = require('path'); // to work with files
+  
+function generateKeyFiles() {
+  
+    const keyPair = crypto.generateKeyPairSync('rsa', {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+            type: 'spki', // for long term storage (recommended in official documentation)
+            format: 'pem'  // Keys stored in pem format
+        },
+        privateKeyEncoding: {
+        type: 'pkcs8', // for long term storage (recommended in official documentation)
+        format: 'pem',  // Keys stored in pem format
+        cipher: 'aes-256-cbc',
+        passphrase: ''
+        }
+    });
+       
+    // Creating public and private key file 
+    fs.writeFileSync("Public_Key_file", keyPair.publicKey);
+    fs.writeFileSync("Private_Key_file", keyPair.privateKey);
+}
+  
+
+generateKeyFiles();   // Run the function
+
+
+
