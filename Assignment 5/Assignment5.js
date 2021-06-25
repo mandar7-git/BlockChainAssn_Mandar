a48350dd3422a0c8295fb6ec461ed7dff31a09ff
mandar7-git <82501714+mandar7-git@users.noreply.github.com>
2021-06-26 00:40:59 +0530

    Will provide stats soon!

---
+++ Assignment 5/Assignment5.js
@@ -0,0 +1,56 @@
+const fs= require("fs");
+const hash= require("crypto-js/sha256");
+const readlineSync= require("readline-sync");
+const path= require('path');
+const time= require('nano-time');
+const crypto = require("crypto");
+const timeBigInt= BigInt(time());
+const SHA256= require('crypto-js/sha256');
+
+var indexOfBlock=readlineSync.question("Enter the index of the block: ");
+var parentHash=readlineSync.question("Enter the hash of the parent block:");
+var targetValue=readlineSync.question("Enter the target value: ");
+
+
+
+var FileName = readlineSync.question("Enter the name of the file storing transaction data: "); 
+var Directory = readlineSync.question("Enter it's directory: ");
+
+var blockData = fs.readFileSync(path.join(Directory,FileName),'utf8'); 
+var blockBodyHash=SHA256(blockData);
+
+var combinedData= indexOfBlock.toString()+parentHash.toString()+blockBodyHash.toString()+targetValue.toString() ;
+
+var i=0;
+var nonce=0 ;
+
+var timeInitial=timeBigInt.toString()
+for ( i=0; ;i++){
+
+    var timestamp=timeBigInt.toString() ;
+    var compare=combinedData+timestamp+i ;
+    var compareHash= crypto.createHash('sha256').update(compare).digest('hex') ;
+
+    if (compareHash<targetValue.toString())
+{
+    nonce=i ;
+    break;
+
+
+}
+}
+
+console.log("Index: ",indexOfBlock);
+console.log("Parent Hash: ",parentHash);
+console.log("Block Body Hash: ",blockBodyHash);
+console.log("Target value: ",targetValue);
+console.log("Timestamp: ", timeBigInt);
+console.log("Nonce: ",nonce);
+
+
+
+
+
+
+
+//console.log(blockData);
